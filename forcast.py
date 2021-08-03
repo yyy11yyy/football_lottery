70573e1472f52ce865ed262a755ba72b4e561388
yyy11yyy <58195817+yyy11yyy@users.noreply.github.com>
2021-08-03 11:10:13 +0800

    Update forcast.py

--- forcast.py
+++ forcast.py
@@ -1,11 +1,3 @@
-from lxml import etree
-import requests
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-import re
-import numpy as np
-from bs4 import NavigableString
-
 def get_row(row_data):
     first_list = []
     for i in row_data.split('\t'):
@@ -36,7 +28,7 @@ def get_second(second_list):
         f10 = fu1 / total1
     return zhusheng1, ping1, fu1, total1, z13, p11, f10
 if __name__ == '__main__':
-    total_flag = 36.7
+    total_flag = 32
     standard = 0.185
     row_datalist = []
     flag = True
@@ -54,21 +46,21 @@ if __name__ == '__main__':
         print('----------------------------------------------------------------------------------------------')
         print('初赔：',round(my_chu[3],3), '\n', '主胜', round(my_chu[4], 3), '平', round(my_chu[5], 3), '客胜', round(my_chu[6], 3))
         print('终赔：', round(my_zhong[3],3), '\n', '主胜', round(my_zhong[4], 3), '平', round(my_zhong[5], 3), '客胜', round(my_zhong[6], 3))
-        if round(my_zhong[0], 3) - round(my_chu[0], 3) < -6.1:
-            print('主胜降盘', round((my_zhong[0] - my_chu[0]), 3))
-        if round(my_zhong[1], 3) - round(my_chu[1], 3) < -6.1:
-            print('平降盘', round((my_zhong[1] - my_chu[1]), 3))
-        if round(my_zhong[2], 3) - round(my_chu[2], 3) < -6.1:
-            print('客胜降盘', round((my_zhong[2] - my_chu[2]), 3))
+        if round(my_zhong[0], 3) - round(my_chu[0], 3) < -20:
+            print('主胜降盘', round((my_zhong[0] - my_chu[0]), 3),'may be 3')
+        if round(my_zhong[1], 3) - round(my_chu[1], 3) < -20:
+            print('平降盘', round((my_zhong[1] - my_chu[1]), 3),'may be 1')
+        if round(my_zhong[2], 3) - round(my_chu[2], 3) < -20:
+            print('客胜降盘', round((my_zhong[2] - my_chu[2]), 3),'may be 0')
         if round(my_zhong[0], 3) - round(my_chu[0], 3) > 10:
-            print('主胜UP', round((my_zhong[0] - my_chu[0]), 3))
+            print('主胜UP', round((my_zhong[0] - my_chu[0]), 3),'主胜UP客胜')
         if round(my_zhong[1], 3) - round(my_chu[1], 3) > 10:
             print('平UP', round((my_zhong[1] - my_chu[1]), 3))
-        if round(my_zhong[2], 3) - round(my_chu[2], 3) > 10:
-            print('客胜UP', round((my_zhong[2] - my_chu[2]), 3))
+        if 10 < round(my_zhong[2], 3) - round(my_chu[2], 3) < 15:
+            print('客胜UP', round((my_zhong[2] - my_chu[2]), 3),'有可能大冷')
         if my_chu[3] > total_flag:
             if (my_chu[0] < my_chu[1] and my_chu[0] < my_chu[2]) and (my_zhong[0] < my_zhong[1] and my_zhong[0] < my_zhong[2]):
-                if my_chu[4] < standard:
+                if my_chu[4] < standard and 0.069<= my_chu[0] <= 0.08:
                     if my_zhong[0] / my_chu[0] > 1.5:
                         print("小心变化")
                     print(3)
@@ -87,7 +79,16 @@ if __name__ == '__main__':
                 continue
             else:
                 print('我是因为standard')
-                print('让球中低赔')
+                print("It's depend on ")
+                print('如果优势方赔率为1.2，可以选低赔!')
+                print('如果优势方赔率为1.4，可以选中高赔!')
+                print('如果优势方赔率为1.5，可以选让球高赔和中赔!')
+                print('如果优势方赔率为1.6，选高低赔')
+                print('如果优势方赔率为1.7，选高低赔')
+                print('如果优势方赔率为1.8，选中低赔')
+                print('如果优势方赔率为1.9，选中低赔')
+                print('如果优势方赔率为2，选中低赔')
         else:
             print('我是因为total')
             print('让球中低赔')
+
